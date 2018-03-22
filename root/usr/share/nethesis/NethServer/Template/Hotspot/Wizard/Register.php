66feb5187f47c29456f6634d708b3ec1c3af4cba
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2018-03-22 10:04:33 +0100

    ui. added description field

--- root/usr/share/nethesis/NethServer/Template/Hotspot/Wizard/Register.php
+++ root/usr/share/nethesis/NethServer/Template/Hotspot/Wizard/Register.php
@@ -5,6 +5,7 @@ echo $view->header('IcaroHost')->setAttribute('template', $T('Register_header'))
 
 echo $view->selector('Id', $view::SELECTOR_DROPDOWN);
 echo $view->textInput('UnitName', $view::STATE_READONLY);
+echo $view->textInput('Description');
 echo $view->selector('Device', $view::SELECTOR_DROPDOWN);
 
 
