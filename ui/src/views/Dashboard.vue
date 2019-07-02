931d2802742005a93bfec67d1fde82bfe158009d
Andrea Leardini <andrea.leardini@nethesis.it>
2019-07-02 18:11:37 +0200

    ui & api. scaffold

---
+++ ui/src/views/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="col-lg-12">
+      <h1>{{$t('dashboard.title')}}</h1>
+      <!-- <div v-show="!configLoaded" class="spinner spinner-lg"></div> -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  props: {
+    msg: String
+  },
+  mounted() {
+  },
+  data() {
+    return {
+      configLoaded: false,
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ ui/src/views/Settings.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{$t('settings.title')}}</h1>
+    <div v-if="!configLoaded" class="spinner spinner-lg"></div>
+    <div v-if="configLoaded">
+      <form class="form-horizontal" v-on:submit.prevent="btSaveClick">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Settings",
+  mounted() {
+  },
+  data() {
+    return {
+      configLoaded: false,
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+</style>
